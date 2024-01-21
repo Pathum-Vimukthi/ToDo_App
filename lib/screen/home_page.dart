c9c1d81c423646818400294c6f1cc1923556a7d1
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-01-21 10:33:49 +0530

    create todo page

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -5,26 +5,50 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController email = TextEditingController();
+    TextEditingController password = TextEditingController();
+
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               const Text(
                 'Login',
                 style: TextStyle(fontSize: 50),
               ),
               const SizedBox(height: 20),
-              const TextField(),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: email,
+                ),
+              ),
               const SizedBox(height: 20),
-              const TextField(),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: TextField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: password,
+                ),
+              ),
               const SizedBox(height: 20),
               SizedBox(
                 width: 200,
                 height: 50,
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    if (email.text == 'admin' && password.text == 'admin') {}
+                  },
                   child: const Text(
                     "Login",
                     style: TextStyle(fontSize: 20),
