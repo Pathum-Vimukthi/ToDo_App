46f6f37bdc647cb4cd7f38e972435e98a0af471e
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-01-21 09:59:49 +0530

    create login page

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -5,6 +5,36 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text(
+                'Login',
+                style: TextStyle(fontSize: 50),
+              ),
+              const SizedBox(height: 20),
+              const TextField(),
+              const SizedBox(height: 20),
+              const TextField(),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
