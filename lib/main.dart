5a85db3aafec2154378086fea086f6d0a3faf629
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2024-01-21 12:16:39 +0530

    list create

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sample_app/screen/home_page.dart';
+import 'package:sample_app/screen/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const ToDoPage(),
     );
   }
 }
--- lib/screen/todo_page.dart
+++ lib/screen/todo_page.dart
@@ -7,15 +7,68 @@ class ToDoPage extends StatefulWidget {
   State<ToDoPage> createState() => _ToDoPageState();
 }
 
+TextEditingController todo = TextEditingController();
+List<String> todos = [];
+
 class _ToDoPageState extends State<ToDoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: const Center(
-        child: Text('ToDo Page'),
+      body: Center(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('ToDo', style: TextStyle(fontSize: 50)),
+              const SizedBox(height: 20),
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    width: 300,
+                    height: 80,
+                    color: Colors.grey.shade200,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("ToDo"),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.check),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        controller: todo,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (todo.text.isNotEmpty) {
+                        todos.add(todo.text);
+                        todo.clear();
+                        print(todos);
+                      }
+                    },
+                    icon: const Icon(Icons.add),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
